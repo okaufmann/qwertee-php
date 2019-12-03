cf0f1c5c4e5ff08330a2979d58296fd62c1c98e3
Oliver Kaufmann <okaufmann91@gmail.com>
2019-12-03 01:04:54 +0100

    add examples

---
+++ example/last-chance.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Okaufmann\QwerteePhp\Qwertee;
+
+
+$today = Qwertee::lastChance();
+echo json_encode($today, JSON_PRETTY_PRINT);
---
+++ example/today.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Okaufmann\QwerteePhp\Qwertee;
+
+
+$today = Qwertee::today();
+var_dump($today);
