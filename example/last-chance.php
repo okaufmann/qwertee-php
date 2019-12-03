d726c642e7e577c2cb67fdbc68eaba72ef8744aa
Oliver Kaufmann <okaufmann91@gmail.com>
2019-12-03 01:07:56 +0100

    fix typo

--- example/last-chance.php
+++ example/last-chance.php
@@ -4,5 +4,5 @@ require_once 'vendor/autoload.php';
 
 use Okaufmann\QwerteePhp\Qwertee;
 
-$today = Qwertee::lastChance();
-echo json_encode($today, JSON_PRETTY_PRINT);
+$lastChance = Qwertee::lastChance();
+echo json_encode($lastChance, JSON_PRETTY_PRINT);
